3cef61cb668d12f083ef32e433ddf72fc2a6a9ea
Taryana <hatakekakasih2002@gmail.com>
2022-09-14 18:05:14 +0700

    feat: routes to home and styling bootstrap

---
+++ src/components/base/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container'>
+      <button className='btn btn-primary'>test</button>
+    </div>
+  )
+}
+
+export default Home
