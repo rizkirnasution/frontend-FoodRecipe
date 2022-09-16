7539805dacac7c3c386980fcf2cd8057b473f244
Rizki Nasution <rizkirnasution@gmail.com>
2022-09-16 07:23:20 +0700

    feat: add router profile, detailVideoRecipe adn addRecipe

--- src/config/routers/index.js
+++ src/config/routers/index.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Home from "../../components/base/Home/Home"
+import Home from "../../components/module/home/Home"
+import Profile from "../../components/module/profile/Profile"
+import DetailVideoRecipe from "../../components/module/detailvideorecipe/index"
+import AddRecipe from "../../components/module/home/addrecipe/AddRecipe"
 
 
 
@@ -9,6 +12,9 @@ const Router = () =>{
             <Routes>
                 <Route path="/" element={<Navigate to="/home" replace="true"/>}/>
                 <Route path="/home" element={<Home/>} />
+                <Route path="/profile" element={<Profile/>} />
+                <Route path="/detailVideoRecipe" element={<DetailVideoRecipe/>} />
+                <Route path="/addRecipe" element={<AddRecipe/>} />
             </Routes>
         </BrowserRouter>
     )
