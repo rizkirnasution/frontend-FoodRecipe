b623b269728719bb91e39fa3b1fd0a37c8656cd0
John Doe <alamsyahbima22@gmail.com>
2022-09-18 18:09:58 +0700

    feat: detail recipe

--- src/config/routers/index.js
+++ src/config/routers/index.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Home from "../../components/base/Home/Home"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "../../components/base/Home/Home";
+import Detail from "../../components/module/Detail/DetailResep";
+import DetailVideo from "../../components/module/Detail/DetailVideo";
 
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace="true" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/Detail" element={<Detail />} />
+        <Route path="/DetailVideo" element={<DetailVideo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-
-const Router = () =>{
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/home" replace="true"/>}/>
-                <Route path="/home" element={<Home/>} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default Router
+export default Router;
