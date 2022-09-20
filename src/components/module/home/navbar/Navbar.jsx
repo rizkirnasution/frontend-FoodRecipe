f0ab8ff4b1a42556292fe78b4e5c5d035ce6040a
John Doe <alamsyahbima22@gmail.com>
2022-09-21 06:06:15 +0700

    add search

--- src/components/module/home/navbar/Navbar.jsx
+++ src/components/module/home/navbar/Navbar.jsx
@@ -1,56 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import user_icon from '../../../../assets/home/User-icon.svg' 
-import './navbar.css'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import user_icon from "../../../../assets/home/User-icon.svg";
+import "./navbar.css";
 
 function Navbar() {
+  const test = useSelector((state) => {
+    console.log(state.auth);
+    return state.auth;
+  });
+  console.log(test);
   return (
     <div className="container">
-        <nav className="navbar  navbar-expand-lg bg-transaparent sticky-top">
+      <nav className="navbar  navbar-expand-lg bg-transaparent sticky-top">
         <div className="container">
-        <a class="navbar-brand" href="/">Food Recipe</a>
-        <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <a class="navbar-brand" href="/">
+            Food Recipe
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-            <div className="collapse navbar-collapse " id="navbarCollapse">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item mx-3">
-                    <a className="nav-link " aria-current="page" href="/home">Home</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link" href="/addRecipe">Add Recipe</a>
-                    </li>
-                    <li className="nav-item mx-3">
-                    <a className="nav-link " href='/profile'>Profile</a>
-                    </li>
-                </ul>
-                <div className="d-flex" >
-                    <div className="container_user ">
-                        <div className="container_user_img mt-4">
-                            <img src={user_icon} alt="" />
-                        </div> 
-                        <span></span>
-                    </div>
-                    <Link to="/login" className='text-decoration-none login'>
-                       <p className='m-auto ms-4 '><a href="#" className='text-decoration-none text-white'>Login</a></p>
-                    </Link>
-                   
+          <div className="collapse navbar-collapse " id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item mx-3">
+                <a className="nav-link " aria-current="page" href="/home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="/addRecipe">
+                  Add Recipe
+                </a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link " href="/profile">
+                  Profile
+                </a>
+              </li>
+            </ul>
+            <div className="d-flex">
+              <div className="container_user ">
+                <div className="container_user_img mt-4">
+                  <img src={user_icon} alt="" />
                 </div>
+                <span></span>
+              </div>
+              {}
+              <Link to="/login" className="text-decoration-none login">
+                <p className="m-auto ms-4 ">
+                  <a href="#" className="text-decoration-none text-white">
+                    Login
+                  </a>
+                </p>
+              </Link>
             </div>
+          </div>
         </div>
-    </nav>
+      </nav>
     </div>
-    
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/module/home/pagination/Searching.jsx
+++ src/components/module/home/pagination/Searching.jsx
@@ -1,74 +1,132 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./search.css"
-import banana_pop from '../../../../assets/home/banana_pop.svg';
-import Footer from "../footer/FooterTopandBottom"
+import React, { useCallback, useEffect, useState } from "react";
+import { Link, Navigate, useNavigate, useSearchParams } from "react-router-dom";
+import "./search.css";
+import banana_pop from "../../../../assets/home/banana_pop.svg";
+import Footer from "../footer/FooterTopandBottom";
 import { Dropdown } from "react-bootstrap";
+import axios from "axios";
+import Card from "../../../base/Card/Card";
+import { useDispatch } from "react-redux";
 
 function Searching() {
+  const [searchParams1, setSearchParams1] = useSearchParams();
+
+  const [sort1, setSort1] = useState("");
+  const [search1, setSearch1] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSort1 = (e) => {
+    setSort1(e.currentTarget.value);
+  };
+
+  function fecth() {
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL + "/recipe?search=" + search1 + "&sortby=title&sortBy=" + sort1 + "&page=1&limit=24")
+      .then((response) => {
+        setRecipes(response.data.data);
+        // console.log(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const handleSearch1 = (e) => {
+    e.preventDefault();
+    fecth();
+    if (search1 !== "" && sort1 !== "") {
+      setSearchParams1({
+        keyword: search1,
+        sort: sort1,
+      });
+    } else if (search1 !== "") {
+      setSearchParams1({
+        keyword: search1,
+      });
+    } else if (sort1 !== "") {
+      setSearchParams1({
+        sort: sort1,
+      });
+    } else {
+      setSearchParams1({});
+    }
+  };
+
+  //   console.log(search)
+  //   console.log(sort)
+
+  useEffect(() => {
+    fecth();
+  }, [searchParams1, sort1]);
+
   return (
     <>
-        <div className="container">
-          <nav className="navbar bg-tranparent mt-3">
-            <div className="container d-flex ">
-              <Link to="/home" className='a-search'>
-                <p className="navbar-brand a-search ">Food Recipe</p>
-              </Link>
-              <div className="d-flex search-container ">
-                <form className="d-flex form-search" role="search">
-                  <input className="form-control input-search " type="search" placeholder="Search" aria-label="Search"/>
-                  <button className="btn btn-warning btn-custom" type="submit">Search</button>
-                </form>
-              </div>
+      <div className="container">
+        <nav className="navbar bg-tranparent mt-3">
+          <div className="container d-flex ">
+            <Link to="/home" className="a-search">
+              <p className="navbar-brand a-search ">Food Recipe</p>
+            </Link>
+            <div className="d-flex search-container ">
+              <form className="d-flex form-search" role="search" onSubmit={handleSearch1}>
+                <input type="text" name="search" placeholder="search" className="form-control input-search " onChange={(e) => setSearch1(e.target.value)} />
+                <button type="submit" className="btn btn-warning btn-custom">
+                  search
+                </button>
+              </form>
             </div>
-            
-          </nav>
-          <div className='my-5'>
-            <Dropdown>
-              <Dropdown.Toggle className='btn-yellow yellow ms-3' variant = "warning" id="dropdown-basic">
-                Sorting Name
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item>
-                  <button className="btn btn-info me-3" >
-                    Name Z-A
-                  </button>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                  <button className="btn btn-success">
-                    Name A-Z
-                  </button>
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
           </div>
-          
-          <div className="container justify-content-center my-5">
-              <div className="row row-cols-sm-2 row-cols-md-3 g-4 ">
-                    <img src={banana_pop} alt=""  />
-                    <img src={banana_pop} alt=""  />
-                    <img src={banana_pop} alt=""  />
-                    <img src={banana_pop} alt=""  />
+        </nav>
+        <div className="my-5">
+          <Dropdown>
+            <Dropdown.Toggle className="btn-yellow yellow ms-3" variant="warning" id="dropdown-basic">
+              Sorting Name
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <button className="ms-3 btn btn-white text-warning me-3" value="ASC" onClick={handleSort1}>
+                  Name Z-A
+                </button>
+              </Dropdown.Item>
+              <div class="dropdown-divider"></div>
+              <Dropdown.Item>
+                <button className="ms-3 btn btn-white text-warning" value="DESC" onClick={handleSort1}>
+                  Name A-Z
+                </button>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        <div className="container justify-content-center my-5">
+          <div className="row row-cols-sm-2 row-cols-md-3 g-4 ">
+            {recipes.length > 0 ? (
+              recipes.map((item) => [
+                <div className="col" key={item.id}>
+                  <Card src={item.thumbnail} titleName={item.title} />
+                </div>,
+              ])
+            ) : (
+              <div className="text-center m-auto  beban ">
+                <h2>Sorry... Data yang anda cari tidak ada</h2>
               </div>
-          </div>
-          <div className="d-flex flex-row my-5 justify-content-evenly">
-            <button className="btn yellow btn-nav ">
-              <span>&larr;</span>
-            </button>
-            <p>
-              {/* {pagination.currentPage}/{pagination.totalPage} */}
-              1/2
-            </p>
-            <button className="btn yellow btn-nav" >
-              &rarr;
-            </button>
+            )}
           </div>
         </div>
-          <Footer/>
+        <div className="d-flex flex-row my-5 justify-content-evenly">
+          <button className="btn yellow btn-nav ">
+            <span>&larr;</span>
+          </button>
+          <p>
+            {/* {pagination.currentPage}/{pagination.totalPage} */}
+            1/2
+          </p>
+          <button className="btn yellow btn-nav">&rarr;</button>
+        </div>
+      </div>
+      <Footer />
     </>
-    
-
-  )
+  );
 }
 
-export default Searching
+export default Searching;
